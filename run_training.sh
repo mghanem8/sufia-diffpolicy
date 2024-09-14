ba1d67c67f1f0d497f3b8256844a3d2e6ffeb745
Nigel Nelson <nigeln@nvidia.com>
2024-09-14 14:24:36 -0700

    Lower number of training runs
    Signed-off-by: Nigel Nelson <nigeln@nvidia.com>

--- run_training.sh
+++ run_training.sh
@@ -19,13 +19,13 @@ HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybr
     exp_name=liver_needle_lift-30-1\
     logging.mode=online &
 
-HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybrid_workspace.yaml" \
-    task=peg_transfer-20 \
-    hydra.run.dir="/lustre/fsw/portfolios/healthcareeng/users/nigeln/diffusion-rgb-multi-cam/peg_transfer-20" \
-    training.seed=0 \
-    training.device="cuda:0" \
-    exp_name=peg_transfer-20-1 \
-    logging.mode=online &
+# HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybrid_workspace.yaml" \
+#     task=peg_transfer-20 \
+#     hydra.run.dir="/lustre/fsw/portfolios/healthcareeng/users/nigeln/diffusion-rgb-multi-cam/peg_transfer-20" \
+#     training.seed=0 \
+#     training.device="cuda:0" \
+#     exp_name=peg_transfer-20-1 \
+#     logging.mode=online &
 
 # GPU 1
 HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybrid_workspace.yaml" \
@@ -44,13 +44,13 @@ HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybr
     exp_name=needle_handover-30-1 \
     logging.mode=online &
 
-HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybrid_workspace.yaml" \
-    task=suture_pad-20 \
-    hydra.run.dir="/lustre/fsw/portfolios/healthcareeng/users/nigeln/diffusion-rgb-multi-cam/suture_pad-20" \
-    training.seed=0 \
-    training.device="cuda:1" \
-    exp_name=suture_pad-20-1 \
-    logging.mode=online &
+# HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybrid_workspace.yaml" \
+#     task=suture_pad-20 \
+#     hydra.run.dir="/lustre/fsw/portfolios/healthcareeng/users/nigeln/diffusion-rgb-multi-cam/suture_pad-20" \
+#     training.seed=0 \
+#     training.device="cuda:1" \
+#     exp_name=suture_pad-20-1 \
+#     logging.mode=online &
 
 # GPU 2
 HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybrid_workspace.yaml" \
@@ -69,21 +69,21 @@ HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybr
     exp_name=peg_transfer-30-1 \
     logging.mode=online &
 
-HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybrid_workspace.yaml" \
-    task=lift_tissue-10 \
-    hydra.run.dir="/lustre/fsw/portfolios/healthcareeng/users/nigeln/diffusion-rgb-multi-cam/lift_tissue-10" \
-    training.seed=0 \
-    training.device="cuda:2" \
-    exp_name=lift_tissue-10-1 \
-    logging.mode=online &
+# HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybrid_workspace.yaml" \
+#     task=lift_tissue-10 \
+#     hydra.run.dir="/lustre/fsw/portfolios/healthcareeng/users/nigeln/diffusion-rgb-multi-cam/lift_tissue-10" \
+#     training.seed=0 \
+#     training.device="cuda:2" \
+#     exp_name=lift_tissue-10-1 \
+#     logging.mode=online &
 
-HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybrid_workspace.yaml" \
-    task=suture_pad-10 \
-    hydra.run.dir="/lustre/fsw/portfolios/healthcareeng/users/nigeln/diffusion-rgb-multi-cam/suture_pad-10" \
-    training.seed=0 \
-    training.device="cuda:2" \
-    exp_name=suture_pad-10 \
-    logging.mode=online &
+# HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybrid_workspace.yaml" \
+#     task=suture_pad-10 \
+#     hydra.run.dir="/lustre/fsw/portfolios/healthcareeng/users/nigeln/diffusion-rgb-multi-cam/suture_pad-10" \
+#     training.seed=0 \
+#     training.device="cuda:2" \
+#     exp_name=suture_pad-10 \
+#     logging.mode=online &
 
 # GPU 3
 HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybrid_workspace.yaml" \
@@ -102,13 +102,13 @@ HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybr
     exp_name=suture_pad-30-1 \
     logging.mode=online &
 
-HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybrid_workspace.yaml" \
-    task=liver_needle_lift-10 \
-    hydra.run.dir="/lustre/fsw/portfolios/healthcareeng/users/nigeln/diffusion-rgb-multi-cam/liver_needle_lift-10" \
-    training.seed=0 \
-    training.device="cuda:3" \
-    exp_name=liver_needle_lift-10-1 \
-    logging.mode=online &
+# HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybrid_workspace.yaml" \
+#     task=liver_needle_lift-10 \
+#     hydra.run.dir="/lustre/fsw/portfolios/healthcareeng/users/nigeln/diffusion-rgb-multi-cam/liver_needle_lift-10" \
+#     training.seed=0 \
+#     training.device="cuda:3" \
+#     exp_name=liver_needle_lift-10-1 \
+#     logging.mode=online &
 
 # GPU 4
 HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybrid_workspace.yaml" \
@@ -127,13 +127,13 @@ HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybr
     exp_name=lift_tissue-20-1 \
     logging.mode=online &
 
-HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybrid_workspace.yaml" \
-    task=liver_needle_lift-20 \
-    hydra.run.dir="/lustre/fsw/portfolios/healthcareeng/users/nigeln/diffusion-rgb-multi-cam/liver_needle_lift-20" \
-    training.seed=0 \
-    training.device="cuda:4" \
-    exp_name=liver_needle_lift-20-1 \
-    logging.mode=online &
+# HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybrid_workspace.yaml" \
+#     task=liver_needle_lift-20 \
+#     hydra.run.dir="/lustre/fsw/portfolios/healthcareeng/users/nigeln/diffusion-rgb-multi-cam/liver_needle_lift-20" \
+#     training.seed=0 \
+#     training.device="cuda:4" \
+#     exp_name=liver_needle_lift-20-1 \
+#     logging.mode=online &
 
 # GPU 5
 HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybrid_workspace.yaml" \
@@ -152,13 +152,13 @@ HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybr
     exp_name=peg_transfer-40-1 \
     logging.mode=online &
 
-HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybrid_workspace.yaml" \
-    task=needle_handover-10 \
-    hydra.run.dir="/lustre/fsw/portfolios/healthcareeng/users/nigeln/diffusion-rgb-multi-cam/needle_handover-10" \
-    training.seed=0 \
-    training.device="cuda:5" \
-    exp_name=needle_handover-10-1 \
-    logging.mode=online &
+# HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybrid_workspace.yaml" \
+#     task=needle_handover-10 \
+#     hydra.run.dir="/lustre/fsw/portfolios/healthcareeng/users/nigeln/diffusion-rgb-multi-cam/needle_handover-10" \
+#     training.seed=0 \
+#     training.device="cuda:5" \
+#     exp_name=needle_handover-10-1 \
+#     logging.mode=online &
 
 # GPU 6
 HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybrid_workspace.yaml" \
@@ -177,13 +177,13 @@ HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybr
     exp_name=suture_pad-40-1 \
     logging.mode=online &
 
-HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybrid_workspace.yaml" \
-    task=peg_transfer-10 \
-    hydra.run.dir="/lustre/fsw/portfolios/healthcareeng/users/nigeln/diffusion-rgb-multi-cam/peg_transfer-10" \
-    training.seed=0 \
-    training.device="cuda:6" \
-    exp_name=peg_transfer-10-1 \
-    logging.mode=online &
+# HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybrid_workspace.yaml" \
+#     task=peg_transfer-10 \
+#     hydra.run.dir="/lustre/fsw/portfolios/healthcareeng/users/nigeln/diffusion-rgb-multi-cam/peg_transfer-10" \
+#     training.seed=0 \
+#     training.device="cuda:6" \
+#     exp_name=peg_transfer-10-1 \
+#     logging.mode=online &
 
 # GPU 7
 HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybrid_workspace.yaml" \
@@ -202,13 +202,13 @@ HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybr
     exp_name=lift_tissue-30-1 \
     logging.mode=online &
 
-HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybrid_workspace.yaml" \
-    task=needle_handover-20 \
-    hydra.run.dir="/lustre/fsw/portfolios/healthcareeng/users/nigeln/diffusion-rgb-multi-cam/liver_nneedle_handovereedle_handover-20" \
-    training.seed=0 \
-    training.device="cuda:7" \
-    exp_name=needle_handover-20-1 \
-    logging.mode=online &
+# HYDRA_FULL_ERROR=1 python train.py --config-name="train_diffusion_unet_ddim_hybrid_workspace.yaml" \
+#     task=needle_handover-20 \
+#     hydra.run.dir="/lustre/fsw/portfolios/healthcareeng/users/nigeln/diffusion-rgb-multi-cam/liver_nneedle_handovereedle_handover-20" \
+#     training.seed=0 \
+#     training.device="cuda:7" \
+#     exp_name=needle_handover-20-1 \
+#     logging.mode=online &
 
 wait
 echo "All processes completed."
